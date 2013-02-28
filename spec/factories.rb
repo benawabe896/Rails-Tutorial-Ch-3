daf41c251fa9908dffceb671dc24604f38a2c56a
Ben Powers <benawabe896@gmail.com>
2013-02-27 18:24:13 -0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Benjamin Powers"
+    email    "benawabe896@gmail.com"
+    password "120412"
+    password_confirmation "120412"
+  end
+end
